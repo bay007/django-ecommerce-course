355687b1e68fdb5baeaf3276cd230da2e8f2195e
egalicia <tnt.galicia@gmail.com>
2018-03-11 22:54:09 -0600

    Add to admin panel

--- productos/models.py
+++ productos/models.py
@@ -7,3 +7,6 @@ class Producto(models.Model):
     titulo = models.CharField(max_length=120)
     descripcion = models.TextField()
     precio = models.DecimalField(decimal_places=2, max_digits=19)
+
+    def __str__(self):
+        return "{}-{}".format(self.titulo, self.precio)
