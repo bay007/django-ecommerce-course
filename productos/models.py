e4e9053b99c8afad8207242bda29fcdca4c3b5ab
egalicia <tnt.galicia@gmail.com>
2018-03-19 23:27:28 -0600

    only Images
    se cambia el campo y se aceptan solo imagenes

--- productos/models.py
+++ productos/models.py
@@ -12,7 +12,7 @@ class Producto(models.Model):
     titulo = models.CharField(max_length=120)
     descripcion = models.TextField()
     precio = models.DecimalField(decimal_places=2, max_digits=19)
-    imagen = models.FileField(upload_to=path_custom, null=True, blank=True)
+    imagen = models.ImageField(upload_to=path_custom, null=True, blank=True)
 
     def __str__(self):
         return "{}-{}".format(self.titulo, self.precio)
