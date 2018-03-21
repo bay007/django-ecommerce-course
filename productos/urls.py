2e14b79861d15b474d8b5f0c269d99f4c3729160
egalicia <tnt.galicia@gmail.com>
2018-03-21 01:02:22 -0600

    minor fix

--- productos/urls.py
+++ productos/urls.py
@@ -3,9 +3,9 @@ from django.conf.urls import url
 from django.contrib import admin
 from .views import ProductoView, producto_list_view, producto_detail_view, ProductoDetail, producto_list_view_featured
 urlpatterns = [
-    url('detailf/(?P<pk>\d+)$', producto_detail_view),
-    url('(?P<pk>\d+)$', ProductoDetail.as_view()),
-    url('listf/$', producto_list_view),
-    url('listfeatured$', producto_list_view_featured),
-    url('$', ProductoView.as_view()),
+    url(r'detailf/(?P<pk>\d+)$', producto_detail_view),
+    url(r'(?P<pk>\d+)$', ProductoDetail.as_view()),
+    url(r'listf/$', producto_list_view),
+    url(r'listfeatured$', producto_list_view_featured),
+    url(r'$', ProductoView.as_view()),
 ]
