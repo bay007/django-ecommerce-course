807a77994b3d729449ba8f977758a9f51a6468fc
egalicia <tnt.galicia@gmail.com>
2018-03-25 17:44:57 -0600

    Clean project
    Dejar solo lo que se usara en adelante en el proyecto

--- productos/urls.py
+++ productos/urls.py
@@ -1,12 +1,8 @@
 
 from django.conf.urls import url
 from django.contrib import admin
-from .views import ProductoView, producto_list_view, producto_detail_view, ProductoDetail, producto_list_view_featured, ProductoDetailSlug
+from .views import ProductoViewList, ProductoDetailSlug
 urlpatterns = [
-    url(r'detailf/(?P<pk>\d+)$', producto_detail_view),
-    url(r'(?P<pk>\d+)$', ProductoDetail.as_view()),
     url(r'(?P<slug>[\w-]+)$', ProductoDetailSlug.as_view()),
-    url(r'listf/$', producto_list_view),
-    url(r'listfeatured$', producto_list_view_featured),
-    url(r'$', ProductoView.as_view()),
+    url(r'$', ProductoViewList.as_view()),
 ]
--- productos/views.py
+++ productos/views.py
@@ -6,47 +6,14 @@ from django.http import Http404
 from productos.models import Producto
 
 
-class ProductoView(ListView):
+class ProductoViewList(ListView):
     queryset = Producto.objects.all()
     template_name = "productos/producto_list.html"
 
     def get_context_data(self, *args, **kwargs):
-        context = super(ProductoView, self).get_context_data(*args, **kwargs)
+        context = super(ProductoViewList, self).get_context_data(*args, **kwargs)
         print(context)
         return context
-
-
-def producto_list_view(request):
-    queryset = Producto.objects.all()
-    print(queryset)
-    context = {
-        'qs': queryset
-    }
-    return render(request, "productos/producto_list.html", context)
-
-
-def producto_list_view_featured(request):
-    queryset = Producto.objects.featured()
-    context = {
-        'object_list': [queryset] if not isinstance(queryset, list) else queryset
-    }
-    return render(request, "productos/producto_list_featured.html", context)
-
-
-class ProductoDetail(DetailView):
-    template_name = "productos/producto_detail.html"
-
-    def get_context_data(self, *args, **kwargs):
-        context = super(ProductoDetail, self).get_context_data(*args, **kwargs)
-        print(context)
-        return context
-
-    def get_object(self, *args, **kwargs):
-        pk = self.kwargs.get('pk')
-        queryset = Producto.objects.obtener_por_id(id=pk)
-        if queryset is None:
-            raise Http404("No existe este objeto :(")
-        return queryset
 
 
 class ProductoDetailSlug(DetailView):
@@ -55,14 +22,3 @@ class ProductoDetailSlug(DetailView):
     def get_object(self):
         slug = self.kwargs.get('slug')
         return get_object_or_404(Producto, slug=slug)
-
-
-def producto_detail_view(request, *args, **kwars):
-    pk = kwars.get("pk")
-    queryset = Producto.objects.obtener_por_id(id=pk)
-    if queryset is None:
-        raise Http404("No existe este objeto")
-    context = {
-        'object': queryset
-    }
-    return render(request, "productos/producto_detail.html", context=context)
