7d2c30cbfd7d1acbafaffc7e5fa24b634fd5f79e
egalicia <tnt.galicia@gmail.com>
2018-04-01 23:00:34 -0500

    Subtotal de la compra
    Con Signals se calcula el subtotal y total de la compra

--- carritos/models.py
+++ carritos/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from productos.models import Producto
 from django.conf import settings
+from django.db.models.signals import m2m_changed
 # Create your models here.
 
 User = settings.AUTH_USER_MODEL
@@ -28,6 +29,8 @@ class CarritoManager(models.Manager):
 class Carrito(models.Model):
     usuario = models.ForeignKey(User, null=True, blank=True)
     productos = models.ManyToManyField(Producto, blank=True)
+    sub_total = models.DecimalField(
+        decimal_places=2, max_digits=8, default=0.00)
     total = models.DecimalField(decimal_places=2, max_digits=8, default=0.00)
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
@@ -36,3 +39,17 @@ class Carrito(models.Model):
 
     def __str__(self):
         return str(self.id)
+
+
+def calcular_subtotal(sender, instance, action, *args, **kwargs):
+    if action in ['post_add', 'post_remove', 'post_clear']:
+        sub_total = 0.0
+        for producto in instance.productos.all():
+            sub_total += float(producto.precio)
+
+        instance.sub_total = sub_total
+        instance.total = float(sub_total)*1.16
+        instance.save()
+
+
+m2m_changed.connect(calcular_subtotal, sender=Carrito.productos.through)
